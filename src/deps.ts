fbe41df46ec1e61828a8b407f1229d0aa416958e
Robin Bühler <git+r@obin.ch>
2023-05-10 09:10:19 +0000

    chore: add configuration parser test

--- src/deps.ts
+++ src/deps.ts
@@ -3,6 +3,7 @@ export * as log from "https://deno.land/std@0.186.0/log/mod.ts";
 export {
   assertEquals,
   assertThrows,
+  assertRejects
 } from "https://deno.land/std@0.186.0/testing/asserts.ts";
 export { stub } from "https://deno.land/std@0.186.0/testing/mock.ts";
 export { z } from "https://deno.land/x/zod@v3.21.4/mod.ts";
---
+++ src/parser/configuration.parser.test.ts
@@ -0,0 +1,29 @@
+import { assertEquals, stub } from "../deps.ts";
+import { Args } from "../models/args.ts";
+import { Configuration } from "../models/configuration.ts";
+import { getConfiguration } from "./configuration.parser.ts";
+
+Deno.test("getConfiguration: should return a valid configuration", async () => {
+  const denoReadTextFileStub = stub(Deno, "readTextFile", () => Promise.resolve(`{
+    "api": "http://localhost:8080/webservice/rest/server.php",
+    "token": "bd9812144e31962db6a37e980b90e3e7",
+    "course": 2
+  }
+  `)
+  )
+
+  const args: Args = {
+    configPath: "test/config.json",
+  };
+
+  const expected: Configuration = {
+    api: "http://localhost:8080/webservice/rest/server.php",
+    token: "bd9812144e31962db6a37e980b90e3e7",
+    course: 2
+  };
+
+  const actual = await getConfiguration(args);
+
+  assertEquals(actual, expected);
+  denoReadTextFileStub.restore();
+});
